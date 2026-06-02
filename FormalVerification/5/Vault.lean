c702a02981ed2261df7710249e30e3e1298e2289
sayyadirfanali <irfan@irfanali.org>
2026-06-02 15:51:50 +0530

    fix bug related to deposit in vault contract

--- FormalVerification/5/Vault.lean
+++ FormalVerification/5/Vault.lean
@@ -20,11 +20,12 @@ open Action
 def contract (s : Vault) (a : Action) : Option Vault :=
   match a with
   | deposit n =>
-    if n >= 0
-    then some { password := s.password, locked := s.locked, amount := s.amount + n }
+    if n >= 0 then
+      if s.locked then some { password := s.password, locked := s.locked, amount := s.amount + n }
+      else none
     else none
-  | claim pw =>
-      if pw = s.password then
+  | claim p =>
+      if p = s.password then
         if s.locked then some { password := s.password, locked := false, amount := 0 }
         else none
       else none
@@ -56,23 +57,34 @@ theorem deposit_preserves_validity : Valid s -> contract s (deposit n) = some s'
     blaster
   · simp [h] at hStep
 
-theorem deposit_preserves_consistency : Consistent s -> contract s (deposit n) = some s' -> Consistent s' := by sorry
+theorem deposit_preserves_consistency :
+  Consistent s -> contract s (deposit n) = some s' -> Consistent s' := by
+  intro hConsistent hStep
+  unfold contract at hStep
+  unfold Consistent at *
+  by_cases hn : n >= 0 <;> simp [hn] at hStep; blaster 
 
-theorem claim_preserves_consistency : contract s (claim pw) = some s' -> Consistent s' := by
-  intro hStep
+theorem claim_preserves_consistency :
+  Consistent s -> contract s (claim p) = some s' -> Consistent s' := by
+  intro hConsistent hStep
   unfold Consistent
   intro hUnlocked
   unfold contract at hStep
-  by_cases hPw : pw = s.password
-  · simp [hPw] at hStep
-    by_cases hLocked : s.locked
-    · simp [hLocked] at hStep
+  by_cases hp : p = s.password
+  · simp [hp] at hStep
+    by_cases hl : s.locked
+    · simp [hl] at hStep
       blaster
-    · simp [hLocked] at hStep
-  · simp [hPw] at hStep
+    · simp [hl] at hStep
+  · simp [hp] at hStep
 
-theorem claim_preserves_validity : Valid s -> contract s (claim p) = some s' -> Valid s' := by sorry
-
+theorem claim_preserves_validity :
+  Valid s -> contract s (claim p) = some s' -> Valid s' := by
+  intro hValid hStep
+  unfold contract at hStep
+  unfold Valid at *
+  by_cases hp : p = s.password <;> simp [hp] at hStep; blaster
+  
 theorem contract_preserves_invariants :
   Valid s ->
   Consistent s ->
@@ -80,18 +92,22 @@ theorem contract_preserves_invariants :
   Valid s' /\ Consistent s' := by
   intro hv hc hs
   cases a with
-  | deposit n => sorry
-  | claim pw => sorry
+  | deposit n => exact ⟨deposit_preserves_validity hv hs, deposit_preserves_consistency hc hs⟩
+  | claim p => exact ⟨claim_preserves_validity hv hs, claim_preserves_consistency hc hs⟩
 
-theorem successful_claim_empties : contract s (claim pw) = some s' -> s'.amount = 0 := by
+theorem successful_claim_empties : contract s (claim p) = some s' -> s'.amount = 0 := by
   intro h
   unfold contract at h
-  by_cases hPw : pw = s.password
-  · simp [hPw] at h
-    by_cases hLocked : s.locked
-    · simp [hLocked] at h
+  by_cases hp : p = s.password
+  · simp [hp] at h
+    by_cases hl : s.locked
+    · simp [hl] at h
       blaster
-    · simp [hLocked] at h
-  · simp [hPw] at h
+    · simp [hl] at h
+  · simp [hp] at h
 
-theorem successful_claim_unlocks : contract s (claim pw) = some s' -> s'.locked = false := by sorry
+theorem successful_claim_unlocks :
+  contract s (claim p) = some s' -> s'.locked = false := by
+  intro hStep
+  unfold contract at hStep
+  by_cases hp : p = s.password <;> simp [hp] at hStep; blaster
